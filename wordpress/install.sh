a102bad33c3854201dd4dd74e49ff1363fad718c
Sven Messmer <sven.messmer@onmail.com>
2022-06-09 13:12:54 +0200

    ../

--- wordpress/install.sh
+++ wordpress/install.sh
@@ -53,8 +53,8 @@ read WP_EMAIL
 WP_EMAIL="${WP_EMAIL:=changeme@example.com}"
 
 #MySQL Install command
-sudo mysql < sql/install.sql
-sudo mysql < sql/yesphpmyadmin.sql
+sudo mysql < ../sql/install.sql
+sudo mysql < ../sql/yesphpmyadmin.sql
 
 #WP CLI
     #MKDIR LOCATION
