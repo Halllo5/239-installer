6aab074b889cd89f0d42a131be2042ff58054c01
Sven Messmer <sven.messmer@onmail.com>
2022-06-11 12:56:47 +0200

    test fix php

--- apache/php/install.sh
+++ apache/php/install.sh
@@ -2,3 +2,14 @@
 
 #Install php
 sudo apt install php libapache2-mod-php php-mysql
+
+#Enable/disable modules
+sudo a2dismod mpm_event && sudo a2enmod mpm_prefork && sudo a2enmod php7.0
+
+#Modify the .conf file
+(sudo cat /etc/apache2/apache2.conf; echo "<FilesMatch \.php$> 
+SetHandler application/x-httpd-php 
+​</FilesMatch>") >> /etc/apache2/apache2.conf
+
+#Restart Apache
+sudo service apache2 restart
